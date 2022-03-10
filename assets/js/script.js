a63829b476aacc5fec87ddeb1dde3f60a32be1a7
Michael Kendrick <michael@locumsmart.net>
2022-03-10 08:00:57 -0600

    need to add tip functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,13 @@ var btn5 = document.querySelector("#btn5");
 
 function doMath(event) {
   var total = document.querySelector("#total");
-
-  total.textContent = "$" + billAmount.value / numPeople.value;
+  btn5 = 1.05;
+  total.textContent = "$" + (billAmount.value * btn5) / numPeople.value;
 }
 
+btn5.addEventListener("click", function (event) {
+  doMath();
+});
 numPeople.addEventListener("keyup", function (event) {
   if (event.keyCode === 13) {
     doMath();
