12cfb27f0b47954e8121411036cbed5d9e86a0bb
Michael Kendrick <michael@locumsmart.net>
2022-03-10 17:29:13 -0600

    styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,66 @@
 var numPeople = document.querySelector("#numPeople");
 var billAmount = document.querySelector("#billAmount");
-var btn5 = document.querySelector("#btn5");
+var btn5El = document.querySelector("#btn5");
+var btn10El = document.querySelector("#btn10");
+var btnsDiv = document.querySelector("#btnsDiv");
+var tipTotal = document.querySelector("#tipTotal");
+var total = document.querySelector("#total");
+var btnReset = document.querySelector("#btnReset");
 
-function doMath(event) {
-  var total = document.querySelector("#total");
-  btn5 = 1.05;
-  total.textContent = "$" + (billAmount.value * btn5) / numPeople.value;
+var tipSelect = document.getElementsByName("tip");
+
+for (var i = tipSelect.length; i--; ) {
+  tipSelect[i].onchange = function () {
+    if (numPeople.value === "") {
+      var totalTip = billAmount.value * this.value;
+      var billSplit = parseInt(billAmount.value) + parseInt(totalTip);
+      total.textContent = "$" + billSplit.toFixed(2);
+      tipTotal.textContent = "$" + totalTip.toFixed(2);
+    } else {
+      var totalTip = billAmount.value * this.value;
+      var billSplit =
+        (parseInt(billAmount.value) + parseInt(totalTip)) / numPeople.value;
+      total.textContent = "$" + billSplit.toFixed(2);
+      var tipSplit = totalTip / numPeople.value;
+      tipTotal.textContent = "$" + tipSplit.toFixed(2);
+    }
+  };
 }
 
-btn5.addEventListener("click", function (event) {
-  doMath();
-});
-numPeople.addEventListener("keyup", function (event) {
-  if (event.keyCode === 13) {
-    doMath();
-  }
-});
+function clearForm() {
+  billAmount.value = "";
+  numPeople.value = "";
+  tipSelect = "";
+  total.textContent = "";
+  tipTotal.textContent = "";
+}
+
+btnReset.addEventListener("click", clearForm);
+
+// btn5El.addEventListener("click", function () {
+//   var tipAmount = parseInt(billAmount.value * btn5);
+//   var totalPlus = (parseInt(billAmount.value) + tipAmount) / numPeople.value;
+//   total.textContent = "$" + totalPlus.toFixed(2);
+//   tipTotal.textContent =
+//     "$" + ((btn5 * billAmount.value) / numPeople.value).toFixed(2);
+//   console.log(billAmount.value);
+//   if (numPeople.value === "") {
+//     var totalPlus = parseInt(billAmount.value) + tipAmount;
+//     total.textContent = "$" + totalPlus.toFixed(2);
+//     tipTotal.textContent = "$" + (btn5 * billAmount.value).toFixed(2);
+//   }
+// });
+
+// btn10El.addEventListener("click", function () {
+//   var tipAmount = parseInt(billAmount.value * btn10);
+//   var totalPlus = (parseInt(billAmount.value) + tipAmount) / numPeople.value;
+//   total.textContent = "$" + totalPlus.toFixed(2);
+//   tipTotal.textContent =
+//     "$" + ((btn10 * billAmount.value) / numPeople.value).toFixed(2);
+//   console.log(btn10);
+//   if (numPeople.value === "") {
+//     var totalPlus = parseInt(billAmount.value) + tipAmount;
+//     total.textContent = "$" + totalPlus.toFixed(2);
+//     tipTotal.textContent = "$" + (btn10 * billAmount.value).toFixed(2);
+//   }
+// });
